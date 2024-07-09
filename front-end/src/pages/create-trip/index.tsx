c8913eb62c4289f41fe178b48fdad401b2ef5aec
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-07-09 17:11:07 -0300

    :sparkles: feat: Adicionar página de detalhes de uma viagem

--- front-end/src/pages/create-trip/index.tsx
+++ front-end/src/pages/create-trip/index.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { InviteGuestsModal } from "../trip-details/invite-guests-modal";
-import { ConfirmTripModal } from "../trip-details/confirm-trip-modal";
+import { InviteGuestsModal } from "./invite-guests-modal";
+import { ConfirmTripModal } from "./confirm-trip-modal";
 import { DestinationAndDateStep } from "./steps/destination-and-date-step";
 import { InviteGuestsStep } from "./steps/invite-guests-step";
 
