7d3644ce325bbd08c736dcf18a2ded97a3e15875
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-07-10 11:44:48 -0300

    :sparkles: feat: Adicionar Zod para validação das requisições

---
+++ back-end/src/routes/create-trip.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { destination, starts_at, ends_at } = request.body;
+      return {
+        destination,
+        starts_at,
+        ends_at,
+      };
+    }
+  );
+}
