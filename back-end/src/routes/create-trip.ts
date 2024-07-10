12c4cfd7b6b32e0cd8229c253e9185af8adc10f5
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-07-10 12:37:14 -0300

    :sparkles: feat: Configurar teste de envio de email usando o nodemailer

--- back-end/src/routes/create-trip.ts
+++ back-end/src/routes/create-trip.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import dayjs from "dayjs";
+import { getMailClient } from "../lib/mail";
+import nodemailer from "nodemailer";
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -11,16 +15,48 @@ export async function createTrip(app: FastifyInstance) {
           destination: z.string().min(4),
           starts_at: z.coerce.date(),
           ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
         }),
       },
     },
     async (request, reply) => {
-      const { destination, starts_at, ends_at } = request.body;
-      return {
-        destination,
-        starts_at,
-        ends_at,
-      };
+      const { destination, starts_at, ends_at, owner_name, owner_email } = request.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Invalid trip start date");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Invalid trip end date");
+      }
+
+      const trip = await prisma.trips.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+        },
+      })
+
+      const main = await getMailClient();
+
+      const massage = await main.sendMail({
+        from: {
+          name: "Equipe plann.er",
+          address: "oi@plann.er",
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: "Sua viagem foi criada!",
+        html: `<p>Parabéns ${owner_name}, sua viagem foi criada com sucesso!</p>`,
+      })
+
+      console.log(nodemailer.getTestMessageUrl(massage))
+
+      return { tripId: trip.id };
     }
   );
 }
